8e4a86eeb3c87c4f59eff0872d38c583f8843b6b
waimea-bsjenner <bsjenner@waimea.school.nz>
2025-03-18 15:09:23 +1300

    got started on a to-do list

--- src/Main.kt
+++ src/Main.kt
@@ -7,6 +7,15 @@
  * GitHub Repo:    https://github.com/waimea-bsjenner/level-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
+ * races to add minimum: Dwarf - sturdy but less fast, Human - goldilocks, Elf - quick but flimsy
+ * classes to add minimum: handheld weapon user, ranged weapon user, magic user
+ * weapons to add minimum: longsword - slow and heavy hitting, rapier - light and quick,
+ *                         longbow - high power arrows, shortbow - lots of small arrows,
+ *                         spellbook - quick cast light spells, staff - slow cast strong spells
+ *                         spells: magic missile - quick, low damage, fireball - slow big damage
+ * movement stuff: system to have distance between 2 players - attacks have certain range
+ *                 walk/run to move
+ *                 dash moves you further but makes you inactive for the next turn
  *
  * =====================================================================
  */
@@ -14,6 +23,5 @@
 
 
 fun main() {
-    println("Hello World!")
 }
 

0b5acea240690173fbf5b00b66287660308176db
waimea-bsjenner <bsjenner@waimea.school.nz>
2025-04-03 12:28:47 +1300

    thank mr copley for helping me cut down A LOT of code

--- src/Main.kt
+++ src/Main.kt
@@ -32,8 +32,14 @@ val LONGBOW = listOf<Int>(40,9,0)
 val QUICKBOW = listOf<Int>(16,9,1)
 val WOODSTAFF = listOf<Int>(45,6,0)
 val SPELLBOOK = listOf<Int>(18,6,1)
-val player1 = getString("What is the name of Player 1? ")
-val player2 = getString("What is the name of Player 2? ")
+const val PLAYERNAME = 0
+const val HEALTH = 1
+const val SPEED = 2
+const val DAMAGE = 3
+const val RANGE = 4
+const val MULTI = 5
+const val POTION = 6
+
 fun main() {
     println(DIVIDER)
     println("BRILLIANT OMEGA OUTSTANDING BATTLE SIMULATOR")
@@ -51,30 +57,47 @@ fun main() {
         }
         else println(ERROR)
     }
-    val p1 = setUpCharacter1Stats()
-    val p2 = setUpCharacter2Stats()
+
+    val p1Name = getPlayerName()
+    val p2Name = getPlayerName()
+
+    val p1Stats = setUpCharacterStats(p1Name)
+    val p2Stats = setUpCharacterStats(p2Name)
+
+    val playerNames = listOf(p1Name, p2Name)
+    val playerStats = listOf(p1Stats, p2Stats)
+
     // (health, movespeed, damage, range, multi attack, health potion number)
     // the loop for attacking
     when ((0..1).random()) {
         0 -> {
-            println("$player1 goes first!")
+            println("${p1Name]} goes first!")
         }
         1 -> {
-            println("$player2 goes first!")
+            println("${p2[PLA
+                    YERNAME]} goes first!")
         }
     }
     var distance = 4
-    while (true) {
+
+    var currentPlayer = 0
+    var currentOpponent = 1
+
+    while (p1[HEALTH] > 0 && p2[HEALTH] > 0) {
+
+        val player = playerStats[currentPlayer]
+        val opponent = playerStats[currentOpponent]
+
         println()
         println("NEXT TURN!")
         println(DIVIDER)
         println("BATTLEFIELD")
         println(DIVIDER)
-        print(player1)
+        print(p1[PLAYERNAME])
         for (i in 1..distance) print("_")
-        println(player2)
+        println(p2[PLAYERNAME])
         println(DIVIDER)
-        println("$player1, choose an action to take.")
+        println("${player[PLAYERNAME]}, choose an action to take.")
         println(DIVIDER)
         println("ATTACK (A)")
         println("MOVE (M)")
@@ -83,112 +106,81 @@ fun main() {
         println(DIVIDER)
         when (readln().uppercase()) {
             "A" -> {
-                if (distance <= p1[3]) {
-                    if (p1[4] == 1) {
+                if (distance <= player[RANGE]) {
+                    if (p1[MULTI] == 1) {
                         val attackNumber = (1..4).random()
-                        println("$player1 attacks $player2 $attackNumber times!".red())
+                        println("${player[PLAYERNAME]} attacks ${opponent[PLAYERNAME]} $attackNumber times!".red())
                         for (i in 1..attackNumber) {
-                            p2[0] -= p1[2]
+                            opponent[HEALTH] -= player[DAMAGE]
                         }
                     }
                     else {
-                        println("$player1 attacks $player2!".red())
-                        p2[0] -= p1[2]
+                        println("${player[PLAYERNAME]} attacks ${opponent[PLAYERNAME]}!".red())
+                        opponent[HEALTH] -= player[DAMAGE]
                     }
                 }
                 else {
-                    println("$player2 is too far away to attack! you miss!")
+                    println("${opponent[PLAYERNAME]} is too far away to attack! you miss!".blue())
                 }
             }
 
             "M" -> {
                 println("Would you like to move left or right? (L/R)")
                 when (readln().uppercase()) {
-                    "L" -> distance += p1[1]
+                    "L" -> distance += player[SPEED]
                     "R" -> {
-                        if (distance < p1[1]) distance = 0
-                        else distance -= p1[1]
+                        if (distance < player[SPEED]) distance = 0
+                        else distance -= player[SPEED]
                     }
                 }
             }
 
             "H" -> {
-                if (p1[5] > 0) {
-                    println("$player1 drinks a health potion!")
-                    p1[0] += (40..50).random()
-                    p1[5] -= 1
+                if (player[POTION] > 0) {
+                    println("${player[PLAYERNAME]} drinks a health potion!".green())
+                    player[HEALTH] += (40..50).random()
+                    player[POTION] -= 1
                 }
                 else {
-                    println("$player1 reaches for a health potion, but there's nothing there!")
+                    println("${player[PLAYERNAME]} reaches for a health potion, but there's nothing there!")
                 }
             }
-        }
-        if (p2[0] <= 0) {
-            println("$player2 is out of health! $player1 wins!")
-            break
-        }
 
-        println()
-        println("NEXT TURN!")
-        println(DIVIDER)
-        println("BATTLEFIELD")
-        println(DIVIDER)
-        print(player1)
-        for (i in 1..distance) print("_")
-        println(player2)
-        println(DIVIDER)
-        println("$player2, choose an action to take.")
-        println(DIVIDER)
-        println("ATTACK (A)")
-        println("MOVE (M)")
-        println("DASH (D)")
-        println("HEAL (H)")
-        println(DIVIDER)
-        when (readln().uppercase()) {
-            "A" -> {
-                if (distance <= p2[3]) {
-                    if (p2[4] == 1) {
-                        val attackNumber = (1..4).random()
-                        println("$player2 attacks $player1 $attackNumber times!".red())
-                        for (i in 1..attackNumber) {
-                            p1[0] -= p2[2]
+            "D" -> {
+                println("Would you like to dash left or right? (L/R)")
+                when (currentPlayer) {
+                    0 -> {
+                        when (readln().uppercase()) {
+                            "L" -> distance += (2 * player[SPEED])
+                            "R" -> {
+                                if (distance < player[SPEED]) distance = 0
+                                else distance -= (2 * player[SPEED])
+                            }
                         }
                     }
-                    else {
-                        println("$player2 attacks $player1!".red())
-                        p1[0] -= p2[2]
+
+                    1 -> {
+                        when (readln().uppercase()) {
+                            "R" -> distance += (2 * player[SPEED])
+                            "L" -> {
+                                if (distance < player[SPEED]) distance = 0
+                                else distance -= (2 * player[SPEED])
+                            }
+                        }
                     }
                 }
-                else {
-                    println("$player1 is too far away to attack! you miss!")
-                }
-            }
 
-            "M" -> {
-                println("Would you like to move left or right? (L/R)")
-                when (readln().uppercase()) {
-                    "R" -> distance += p2[1]
-                    "L" -> {
-                        if (distance < p2[1]) distance = 0
-                        else distance -= p2[1]
-                    }
-                }
-            }
-
-            "H" -> {
-                if (p2[5] > 0) {
-                    println("$player2 drinks a health potion!")
-                    p2[0] += (40..50).random()
-                    p2[5] -= 1
-                }
-                else {
-                    println("$player2 reaches for a health potion, but there's nothing there!")
-                }
             }
         }
-        if (p1[0] <= 0) {
-            println("$player1 is out of health! $player2 wins!")
-            break
+        when (currentPlayer) {
+            1 -> {
+                currentPlayer = 0
+                currentOpponent = 1
+            }
+            0 -> {
+                currentOpponent = 0
+                currentPlayer = 1
+            }
         }
     }
 }
@@ -230,55 +222,36 @@ fun chooseWeapon(prompt: String): String {
         else println(ERROR)
     }
 }
-fun setUpCharacter2Stats(): MutableList<Int> {
-    val p2 = mutableListOf<Int>()
-    val p2Race = chooseRace("$player2, choose a race of character")
-    println("$player2 has picked $p2Race")
-    val p2Weapon = chooseWeapon("$player2, choose a weapon")
-    println("$player2 has picked $p2Weapon")
-    when (p2Race) {
-        "Dwarf" -> p2.add(DWARF[0]) && p2.add(DWARF[1])
-        "Elf" -> p2.add(ELF[0]) && p2.add(ELF[1])
-        "Human" -> p2.add(HUMAN[0]) && p2.add(HUMAN[1])
+
+fun getPlayerName(): String {
+    return getString("What is the name of player 2?")
+}
+
+fun setUpCharacterStats(name: String): MutableList<Int> {
+    val stats = mutableListOf<Int>()
+
+    val race = chooseRace("${name}, choose a race of character")
+    println("${name} has picked $race")
+    val weapon = chooseWeapon("${name}, choose a weapon")
+    println("${name} has picked $weapon")
+    when (race) {
+        "Dwarf" -> stats.add(DWARF[0]) && stats.add(DWARF[1])
+        "Elf" -> stats.add(ELF[0]) && stats.add(ELF[1])
+        "Human" -> stats.add(HUMAN[0]) && stats.add(HUMAN[1])
     }
-    when (p2Weapon) {
-        "Longsword" -> p2.add(LONGSWORD[0]) && p2.add(LONGSWORD[1]) && p2.add(LONGSWORD[2])
-        "Rapier" -> p2.add(RAPIER[0]) && p2.add(RAPIER[1]) && p2.add(RAPIER[2])
-        "Longbow" -> p2.add(LONGBOW[0]) && p2.add(LONGBOW[1]) && p2.add(LONGBOW[2])
-        "Quickbow" -> p2.add(QUICKBOW[0]) && p2.add(QUICKBOW[1]) && p2.add(QUICKBOW[2])
-        "Woodstaff" -> p2.add(WOODSTAFF[0]) && p2.add(WOODSTAFF[1]) && p2.add(WOODSTAFF[2])
-        "Spellbook" -> p2.add(SPELLBOOK[0]) && p2.add(SPELLBOOK[1]) && p2.add(SPELLBOOK[2])
+    when (weapon) {
+        "Longsword" -> stats.add(LONGSWORD[0]) && stats.add(LONGSWORD[1]) && stats.add(LONGSWORD[2])
+        "Rapier" -> stats.add(RAPIER[0]) && stats.add(RAPIER[1]) && stats.add(RAPIER[2])
+        "Longbow" -> stats.add(LONGBOW[0]) && stats.add(LONGBOW[1]) && stats.add(LONGBOW[2])
+        "Quickbow" -> stats.add(QUICKBOW[0]) && stats.add(QUICKBOW[1]) && stats.add(QUICKBOW[2])
+        "Woodstaff" -> stats.add(WOODSTAFF[0]) && stats.add(WOODSTAFF[1]) && stats.add(WOODSTAFF[2])
+        "Spellbook" -> stats.add(SPELLBOOK[0]) && stats.add(SPELLBOOK[1]) && stats.add(SPELLBOOK[2])
     }
 
-    p2.add(2)
-    return p2
+    stats.add(2)
+    return stats
 }
-fun setUpCharacter1Stats(): MutableList<Int> {
-    val p1 = mutableListOf<Int>()
-    // ask p1 for their character's race
-    val p1Race = chooseRace("$player1, choose a race of character")
-    println("$player1 has picked $p1Race")
-    // ask p1 for their character's weapon
-    val p1Weapon = chooseWeapon("$player1, choose a weapon")
-    println("$player1 has picked $p1Weapon")
-    // give appropriate stats in the list form of (health, movement speed, damage, attack range)
-    when (p1Race) {
-        "Dwarf" -> p1.add(DWARF[0]) && p1.add(DWARF[1])
-        "Elf" -> p1.add(ELF[0]) && p1.add(ELF[1])
-        "Human" -> p1.add(HUMAN[0]) && p1.add(HUMAN[1])
-    }
 
-    when (p1Weapon) {
-        "Longsword" -> p1.add(LONGSWORD[0]) && p1.add(LONGSWORD[1]) && p1.add(LONGSWORD[2])
-        "Rapier" -> p1.add(RAPIER[0]) && p1.add(RAPIER[1]) && p1.add(RAPIER[2])
-        "Longbow" -> p1.add(LONGBOW[0]) && p1.add(LONGBOW[1]) && p1.add(LONGBOW[2])
-        "Quickbow" -> p1.add(QUICKBOW[0]) && p1.add(QUICKBOW[1]) && p1.add(QUICKBOW[2])
-        "Woodstaff" -> p1.add(WOODSTAFF[0]) && p1.add(WOODSTAFF[1]) && p1.add(WOODSTAFF[2])
-        "Spellbook" -> p1.add(SPELLBOOK[0]) && p1.add(SPELLBOOK[1]) && p1.add(SPELLBOOK[2])
-    }
-    p1.add(2)
-    return p1
-}
 
 fun getString(prompt: String): String {
     var userInput: String
